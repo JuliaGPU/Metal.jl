b460f9d1ec961bb8af7bf213ad792928364e2820
Tim Besard <tim.besard@gmail.com>
2021-11-23 02:15:27 -0500

    Fix package build.

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,10 @@
-mkpath("build")
-cd("build")
-run(`cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=../ ../../cmt/`)
-run(`make -j20 VERBOSE=1`)
-cd("..")
-rm("build", recursive=true, force=true)
+sourcedir = joinpath(dirname(@__DIR__), "cmt")
+
+builddir = joinpath(@__DIR__, "build")
+if isdir(builddir)
+    rm(builddir; recursive=true)
+end
+mkdir(builddir)
+
+run(`cmake -S $sourcedir -B $builddir -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$(@__DIR__) -DCMAKE_BUILD_TYPE=Debug`)
+run(`make -C $builddir -j$(Sys.CPU_THREADS) VERBOSE=1`)
