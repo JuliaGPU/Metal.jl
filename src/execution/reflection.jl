201c7a1fed1fdeb486ad99c49fae1da095395f1a
Max Hawkins <max.hawkins65@gmail.com>
2022-02-28 10:24:37 -0600

    Add reflection

---
+++ src/execution/reflection.jl
@@ -0,0 +1,61 @@
+# code reflection entry-points
+
+# forward the rest to GPUCompiler with an appropriate CompilerJob
+# TODO: Actually implement this for Metal
+for method in (:code_typed, :code_warntype, :code_llvm, :code_native)
+    # only code_typed doesn't take a io argument
+    args = method == :code_typed ? (:job,) : (:io, :job)
+
+    @eval begin
+        function $method(io::IO, @nospecialize(func), @nospecialize(types);
+                         kernel::Bool=false, minthreads=nothing, maxthreads=nothing,
+                         blocks_per_sm=nothing, maxregs=nothing, kwargs...)
+            source = FunctionSpec(func, Base.to_tuple_type(types), kernel)
+            # target = CUDACompilerTarget(device(); minthreads, maxthreads, blocks_per_sm, maxregs)
+            target = MetalCompilerTarget(macos=get_macos_v();)
+            params = MetalCompilerParams()
+            job = CompilerJob(target, source, params)
+            GPUCompiler.$method($(args...); kwargs...)
+        end
+        $method(@nospecialize(func), @nospecialize(types); kwargs...) =
+            $method(stdout, func, types; kwargs...)
+    end
+end
+
+"""
+    Metal.return_type(f, tt) -> r::Type
+
+Return a type `r` such that `f(args...)::r` where `args::tt`.
+"""
+function return_type(@nospecialize(func), @nospecialize(tt))
+    source = FunctionSpec(func, tt, true)
+    # target = CUDACompilerTarget(device())
+    target = MetalCompilerTarget(macos=get_macos_v();)
+    params = MetalCompilerParams()
+    job = CompilerJob(target, source, params)
+    interp = GPUCompiler.get_interpreter(job)
+    if VERSION >= v"1.8-"
+        sig = Base.signature_type(job.source.f, job.source.tt)
+        Core.Compiler.return_type(interp, sig)
+    else
+        Core.Compiler.return_type(interp, job.source.f, job.source.tt)
+    end
+end
+
+
+#
+# @device_code_* functions
+#
+
+export @device_code_lowered, @device_code_typed, @device_code_warntype,
+       @device_code_llvm, @device_code
+
+
+# forward the rest to GPUCompiler
+@eval $(Symbol("@device_code_lowered")) = $(getfield(GPUCompiler, Symbol("@device_code_lowered")))
+@eval $(Symbol("@device_code_typed")) = $(getfield(GPUCompiler, Symbol("@device_code_typed")))
+@eval $(Symbol("@device_code_warntype")) = $(getfield(GPUCompiler, Symbol("@device_code_warntype")))
+@eval $(Symbol("@device_code_llvm")) = $(getfield(GPUCompiler, Symbol("@device_code_llvm")))
+# Is there a Metal equivalent?
+# @eval $(Symbol("@device_code_ptx")) = $(getfield(GPUCompiler, Symbol("@device_code_native")))
+@eval $(Symbol("@device_code")) = $(getfield(GPUCompiler, Symbol("@device_code")))
