63d67af724b80893e43dcf9686e2fb8ea3763ecb
Max Hawkins <max.hawkins65@gmail.com>
2022-03-10 12:55:23 -0800

    Refactor device_code macros

--- src/execution/reflection.jl
+++ src/execution/reflection.jl
@@ -40,63 +40,6 @@ function return_type(@nospecialize(func), @nospecialize(tt))
     end
 end
 
-"""
-    code_metallib([io], f, types)
-
-Writes the Metal library code generated for the method matching the given generic function and type
-signature to a file.
-
-The following keyword arguments are supported:
-
-- `kernel`: treat the function as an entry-point kernel
-- `filename`: the file to write the metal library to
-
-See also: [`@device_code_metallib`](@ref)
-"""
-function code_metallib(io::IO, @nospecialize(func), @nospecialize(types), kernel::Bool=true;
-                   filename::String="", kwargs...)
-    tt = Base.to_tuple_type(types)
-    source = FunctionSpec(f, tt, true, name)
-    target = MetalCompilerTarget(macos=get_macos_v(); kwargs...)
-    params = MetalCompilerParams()
-    job = CompilerJob(target, source, params)
-    code_metallib(io, job; filename=filename)
-end
-
-function code_metallib(io::IO, job::CompilerJob{MetalCompilerTarget}; filename::String="")
-    if !job.source.kernel
-        error("Can only generate a Metal library for kernel functions")
-    end
-    
-    compiled = mtlfunction_compile(job)
-    if filename == ""
-       filename = tempname() * ".metallib" 
-    end
-
-    open(filename, "w") do file
-        write(file, compiled.image)
-    end
-    println(io, "Metal library saved to $filename")
-end
-
-code_metallib(@nospecialize(func), @nospecialize(types); kwargs...) =
-    code_metallib(stdout, func, types; kwargs...)
-
-"""
-    @device_code_metallib [io::IO=stdout, ...] ex
-
-Evaluates the expression `ex` and writes the result of [`Metal.code_metallib`](@ref) to
-a file for every compiled Metal kernel. For other supported keywords, see
-[`Metal.code_metallib`](@ref).
-"""
-macro device_code_metallib(ex...)
-    function hook(job::CompilerJob{MetalCompilerTarget}; io::IO=stdout, kwargs...)
-        println(io, "// $job")
-        println(io)
-        code_metallib(io, job; kwargs...)
-    end
-    GPUCompiler.emit_hooked_compilation(hook, ex...)
-end
 
 #
 # @device_code_* functions
@@ -111,4 +54,5 @@ export @device_code_lowered, @device_code_typed, @device_code_warntype,
 @eval $(Symbol("@device_code_typed")) = $(getfield(GPUCompiler, Symbol("@device_code_typed")))
 @eval $(Symbol("@device_code_warntype")) = $(getfield(GPUCompiler, Symbol("@device_code_warntype")))
 @eval $(Symbol("@device_code_llvm")) = $(getfield(GPUCompiler, Symbol("@device_code_llvm")))
+@eval $(Symbol("@device_code_metallib")) = $(getfield(GPUCompiler, Symbol("@device_code_native")))
 @eval $(Symbol("@device_code")) = $(getfield(GPUCompiler, Symbol("@device_code")))
