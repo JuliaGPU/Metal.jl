58c3e97e8e8ad82651555ce33d75e2999b8ec363
Max Hawkins <max.hawkins65@gmail.com>
2022-04-19 13:21:35 +0200

    Clarify dependency on custom GPUCompiler on error

--- src/execution/gpucompiler.jl
+++ src/execution/gpucompiler.jl
@@ -1,3 +1,9 @@
+# While Metal.jl is still in development, clarify undefined MetalCompilerTarget error
+isdefined(GPUCompiler, :MetalCompilerTarget) ||
+    error("MetalCompilerTarget is undefined. \
+            A special fork of GPUCompiler is currently required for this package. The link is in the top-level README. \
+            Dev that GPUCompiler version then try precompiling Metal.jl again.")
+
 const ci_cache = GPUCompiler.CodeCache()
 
 struct MetalCompilerParams <: GPUCompiler.AbstractCompilerParams end
