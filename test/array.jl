99643afa011c3f76153b9c31acc987aab492f1a7
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-09-08 00:40:03 -0400

    Add aliases MtlVector and MtlMatrix

--- test/array.jl
+++ test/array.jl
@@ -14,18 +14,36 @@ end
 
 @testset "fill($T)" for T in [Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64,
                               Float32]
-    A = MtlArray{T}(undef, (10, 10))
+
+    A = MtlArray{T}(undef, (10, 10, 10))
     b = rand(T)
-    A = Metal.fill(b, (10, 10))
+    A = Metal.fill(b, (10, 10, 10))
     @test all(Array(A) .== b)
+
+    M = MtlMatrix{T}(undef, (10, 10))
+    M = Metal.fill(b, (10, 10))
+    @test all(Array(M) .== b)
+
+    v = MtlVector{T}(undef, 10)
+    v = Metal.fill(b, 10)
+    @test all(Array(v) .== b)
 end
 
 @testset "fill!($T)" for T in [Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64,
                                Float32]
-    A = MtlArray{T}(undef, (10, 10))
+
+    A = MtlArray{T}(undef, (10, 10, 10))
     b = rand(T)
     fill!(A, b)
     @test all(Array(A) .== b)
+
+    M = MtlMatrix{T}(undef, (10, 10))
+    fill!(M, b)
+    @test all(Array(M) .== b)
+
+    v = MtlVector{T}(undef, 10)
+    fill(v, b)
+    @test all(Array(v) .== b)
 end
 
 end
