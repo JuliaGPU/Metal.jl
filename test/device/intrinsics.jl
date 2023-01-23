57f5575c79e58e5880b032f352f1cfe1981b4ba3
Tim Besard <tim.besard@gmail.com>
2023-01-23 12:37:57 +0100

    Reorganize intrinsic tests (NFC).

--- test/device/intrinsics.jl
+++ test/device/intrinsics.jl
@@ -202,55 +202,66 @@ end
 
 @testset "simd intrinsics" begin
 
-    @testset "simd shuffle" begin
-        shuffle_typs = [Float32, Float16, Int32, UInt32, Int16, UInt16, Int8, UInt8]
-        @testset for typ in shuffle_typs
+@testset "shuffle($typ)" for typ in [Float32, Float16, Int32, UInt32, Int16, UInt16, Int8, UInt8]
+    function kernel(a::MtlDeviceVector{T}, b::MtlDeviceVector{T}) where T
+        idx = thread_position_in_grid_1d()
+        idx_in_simd = thread_index_in_simdgroup()
+        simd_idx = simdgroup_index_in_threadgroup()
 
-            function shuffle_down_kernel(a::MtlDeviceVector{T}, b::MtlDeviceVector{T}) where T
-                idx = thread_position_in_grid_1d()
-                idx_in_simd = thread_index_in_simdgroup()
-                simd_idx = simdgroup_index_in_threadgroup()
+        temp = MtlThreadGroupArray(T, 32)
+        temp[idx] = a[idx]
+        simdgroup_barrier(Metal.MemoryFlagThreadGroup)
 
-                temp = MtlThreadGroupArray(T, 32)
-                temp[idx] = a[idx]
-                simdgroup_barrier(Metal.MemoryFlagThreadGroup)
+        if simd_idx == 1
+            value = temp[idx_in_simd]
 
-                if simd_idx == 1
-                    value = temp[idx_in_simd]
+            value = value + simd_shuffle_down(value, 16)
+            value = value + simd_shuffle_down(value,  8)
+            value = value + simd_shuffle_down(value,  4)
+            value = value + simd_shuffle_down(value,  2)
+            value = value + simd_shuffle_down(value,  1)
 
-                    value = value + simd_shuffle_down(value, 16)
-                    value = value + simd_shuffle_down(value,  8)
-                    value = value + simd_shuffle_down(value,  4)
-                    value = value + simd_shuffle_down(value,  2)
-                    value = value + simd_shuffle_down(value,  1)
+            b[idx] = value
+        end
+        return
+    end
 
-                    b[idx] = value
-                end
-                return
-            end
+    dev_a = MtlArray{typ}(undef, 32)
+    dev_b = MtlArray{typ}(undef, 32)
+    a = unsafe_wrap(Array{typ}, dev_a, 32)
+    b = unsafe_wrap(Array{typ}, dev_b, 32)
 
-            dev_a = MtlArray{typ}(undef, 32)
-            dev_b = MtlArray{typ}(undef, 32)
-            a = unsafe_wrap(Array{typ}, dev_a, 32)
-            b = unsafe_wrap(Array{typ}, dev_b, 32)
+    rand!(a, (1:4))
+    Metal.@sync @metal threads=32 kernel(dev_a, dev_b)
+    @test sum(a) ≈ b[1]
+end
 
-            rand!(a, (1:4))
-            Metal.@sync @metal threads=32 shuffle_down_kernel(dev_a, dev_b)
-            @test sum(a) ≈ b[1]
-        end # End SIMD shuffle types
-    end # End SIMD Shuffle intrinsics
-
-@testset "simd matrix functions" begin
-    typs = [Float16, Float32]
-    @testset for typ in typs
-        function load_store(a::MtlDeviceArray{T}, b::MtlDeviceArray{T},
+@testset "matrix functions" begin
+    @testset "load_store($typ)" for typ in [Float16, Float32]
+        function kernel(a::MtlDeviceArray{T}, b::MtlDeviceArray{T},
                             origin_a=(1, 1), origin_b=(1, 1)) where {T}
             sg_a = simdgroup_load(a, origin_a)
             simdgroup_store(sg_a, b, origin_b)
             return
         end
 
-        function load_store_tg(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}) where {T}
+        let
+            a = MtlArray(rand(typ, 8, 8))
+            b = MtlArray(zeros(typ, 8, 8))
+            @metal threads=(8, 8) kernel(a, b)
+            @test Array(a) == Array(b)
+        end
+
+        let
+            a = MtlArray(rand(typ, 20, 15))
+            b = MtlArray(zeros(typ, 15, 20))
+            @metal threads=(8, 8) kernel(a, b, (4, 2), (3, 5))
+            @test Array(a)[4:11, 2:9] == Array(b)[3:10, 5:12]
+        end
+    end
+
+    @testset "load_store_tg($typ)" for typ in [Float16, Float32]
+        function kernel(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}) where {T}
             pos = thread_position_in_threadgroup_2d()
 
             tg_a = MtlThreadGroupArray(T, (8, 8))
@@ -264,7 +275,14 @@ end
             return
         end
 
-        function mul(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}, c::MtlDeviceArray{T}) where {T}
+        a = MtlArray(rand(typ, 8, 8))
+        b = MtlArray(zeros(typ, 8, 8))
+        @metal threads=(8, 8) kernel(a, b)
+        @test Array(a) == Array(b)
+    end
+
+    @testset "mul($typ)" for typ in [Float16, Float32]
+        function kernel(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}, c::MtlDeviceArray{T}) where {T}
             sg_a = simdgroup_load(a)
             sg_b = simdgroup_load(b)
             sg_c = simdgroup_multiply(sg_a, sg_b)
@@ -272,7 +290,16 @@ end
             return
         end
 
-        function mad(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}, c::MtlDeviceArray{T}, d::MtlDeviceArray{T}) where {T}
+        a = MtlArray(rand(typ, 8, 8))
+        b = MtlArray(rand(typ, 8, 8))
+        c = MtlArray(zeros(typ, 8, 8))
+        @metal threads=(8, 8) kernel(a, b, c)
+        @test Array(a) * Array(b) ≈ Array(c)
+    end
+
+    @testset "mad($typ)" for typ in [Float16, Float32]
+        function kernel(a::MtlDeviceArray{T}, b::MtlDeviceArray{T}, c::MtlDeviceArray{T},
+                    d::MtlDeviceArray{T}) where {T}
             sg_a = simdgroup_load(a)
             sg_b = simdgroup_load(b)
             sg_c = simdgroup_load(c)
@@ -282,32 +309,12 @@ end
         end
 
         a = MtlArray(rand(typ, 8, 8))
-        b = MtlArray(zeros(typ, 8, 8))
-        @metal threads=(8, 8) load_store(a, b)
-        @test Array(a) == Array(b)
-
-        a = MtlArray(rand(typ, 8, 8))
-        b = MtlArray(zeros(typ, 8, 8))
-        @metal threads=(8, 8) load_store_tg(a, b)
-        @test Array(a) == Array(b)
-
-        a = MtlArray(rand(typ, 20, 15))
-        b = MtlArray(zeros(typ, 15, 20))
-        @metal threads=(8, 8) load_store(a, b, (4, 2), (3, 5))
-        @test Array(a)[4:11, 2:9] == Array(b)[3:10, 5:12]
-
-        a = MtlArray(rand(typ, 8, 8))
-        b = MtlArray(rand(typ, 8, 8))
-        c = MtlArray(zeros(typ, 8, 8))
-        @metal threads=(8, 8) mul(a, b, c)
-        @test Array(a) * Array(b) ≈ Array(c)
-
-        a = MtlArray(rand(typ, 8, 8))
         b = MtlArray(rand(typ, 8, 8))
         c = MtlArray(rand(typ, 8, 8))
         d = MtlArray(zeros(typ, 8, 8))
-        @metal threads=(8, 8) mad(a, b, c, d)
+        @metal threads=(8, 8) kernel(a, b, c, d)
         @test Array(a) * Array(b) + Array(c) ≈ Array(d)
     end
 end # End Matrix Functions
+
 end # End SIMD Intrinsics
