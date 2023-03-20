a8ea450ae53558675fea6602571401af3d8bb790
Max Ng <maxwindiff@gmail.com>
2023-03-19 17:37:00 -0700

    Skip unsupported test

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -9,5 +9,6 @@ using Metal.MetalKernels
 # TODO: check if Metal is functional (depends on https://github.com/JuliaGPU/Metal.jl/issues/121)
 Metal.versioninfo()
 Metal.allowscalar(false)
-Testsuite.testsuite(()->MetalBackend(), "Metal", Metal, MtlArray, Metal.MtlDeviceArray)
+Testsuite.testsuite(()->MetalBackend(), "Metal", Metal, MtlArray, Metal.MtlDeviceArray;
+  skip_tests=Set(["Convert"]))
 Testsuite.unittest_testsuite(()->MetalBackend(), "Metal", Metal, Metal.MtlDeviceArray)
