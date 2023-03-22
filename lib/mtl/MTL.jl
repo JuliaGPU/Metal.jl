7716084b62f9fc79a64bb8b9be5ca436424d47c9
Christian Guinard <chguinard99@gmail.com>
2023-03-21 22:02:53 -0300

    Add MTLGPUSupport and supports_family function

--- lib/mtl/MTL.jl
+++ lib/mtl/MTL.jl
@@ -45,6 +45,7 @@ include("command_enc/blit.jl")
 include("command_enc/compute.jl")
 include("binary_archive.jl")
 include("capture.jl")
+include("family.jl")
 
 function __init__()
     precompiling = ccall(:jl_generating_output, Cint, ()) != 0
