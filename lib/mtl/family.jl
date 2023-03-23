5163674d2a9c9ebca3f04553b39d45b5589e4913
Christian Guinard <chguinard99@gmail.com>
2023-03-23 15:51:14 -0300

    Follow Metal naming convention

--- lib/mtl/family.jl
+++ lib/mtl/family.jl
@@ -1,22 +1,22 @@
 export supports_family
 
 @cenum MTLGPUFamily::NSUInteger begin
-    metal3 = 5001 # Metal 3 support
+    MTLGPUFamilyMetal3 = 5001 # Metal 3 support
 
-    apple8 = 1008 # M2 & A15
-    apple7 = 1007 # M1 & A14
-    apple6 = 1006 #      A13
-    apple5 = 1005 #      A12
-    apple4 = 1004 #      A11
-    apple3 = 1003 # A9 & A10
-    apple2 = 1002 #      A8
-    apple1 = 1001 #      A7
+    MTLGPUFamilyApple8 = 1008 # M2 & A15
+    MTLGPUFamilyApple7 = 1007 # M1 & A14
+    MTLGPUFamilyApple6 = 1006 #      A13
+    MTLGPUFamilyApple5 = 1005 #      A12
+    MTLGPUFamilyApple4 = 1004 #      A11
+    MTLGPUFamilyApple3 = 1003 # A9 & A10
+    MTLGPUFamilyApple2 = 1002 #      A8
+    MTLGPUFamilyApple1 = 1001 #      A7
 
-    common3 = 3003
-    common2 = 3002
-    common1 = 3001
+    MTLGPUFamilyCommon3 = 3003
+    MTLGPUFamilyCommon2 = 3002
+    MTLGPUFamilyCommon1 = 3001
 
-    mac2 = 2002 # Mac family 2 GPU features
+    MTLGPUFamilyMac2 = 2002 # Mac family 2 GPU features
 end
 
 function supports_family(device::MTLDevice, gpufamily::MTLGPUFamily)
