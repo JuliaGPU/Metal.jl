5c93f92ad14e3600fcdada3b8a7b856dc1986691
Max Ng <maxwindiff@gmail.com>
2023-03-24 17:40:01 -0700

    Update test/kernelabstractions.jl
    Co-authored-by: Tim Besard <tim.besard@gmail.com>

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -3,12 +3,6 @@ using Test
 
 include(joinpath(dirname(pathof(KernelAbstractions)), "..", "test", "testsuite.jl"))
 
-using Metal
-using Metal.MetalKernels
-
-# TODO: check if Metal is functional (depends on https://github.com/JuliaGPU/Metal.jl/issues/121)
-Metal.versioninfo()
-Metal.allowscalar(false)
 Testsuite.testsuite(()->MetalBackend(), "Metal", Metal, MtlArray, Metal.MtlDeviceArray; skip_tests=Set([
     "Convert",           # depends on https://github.com/JuliaGPU/Metal.jl/issues/69
     "SpecialFunctions",  # no equivalent Metal intrinsics for gamma, erf, etc
