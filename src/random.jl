399596d9f98674beafa9b9044b8cb857eb13082a
Tim Gymnich <tgymnich@icloud.com>
2023-05-26 15:41:20 +0200

    add storage kwarg

--- src/random.jl
+++ src/random.jl
@@ -7,18 +7,18 @@ Random.rand!(A::MtlArray) = Random.rand!(gpuarrays_rng(), A)
 Random.randn!(A::MtlArray) = Random.randn!(gpuarrays_rng(), A)
 
 # GPUArrays out-of-place
-rand(T::Type, dims::Dims) = Random.rand!(MtlArray{T}(undef, dims...))
-randn(T::Type, dims::Dims; kwargs...) = Random.randn!(MtlArray{T}(undef, dims...); kwargs...)
+rand(T::Type, dims::Dims; storage::MTL.MTLResourceOptions=Private) = Random.rand!(MtlArray{T}(undef, dims...; storage))
+randn(T::Type, dims::Dims; storage::MTL.MTLResourceOptions=Private, kwargs...) = Random.randn!(MtlArray{T}(undef, dims...; storage); kwargs...)
 
 # support all dimension specifications
-rand(T::Type, dim1::Integer, dims::Integer...) =
-    Random.rand!(MtlArray{T}(undef, dim1, dims...))
-randn(T::Type, dim1::Integer, dims::Integer...; kwargs...) =
-    Random.randn!(MtlArray{T}(undef, dim1, dims...); kwargs...)
+rand(T::Type, dim1::Integer, dims::Integer...; storage::MTL.MTLResourceOptions=Private) =
+    Random.rand!(MtlArray{T}(undef, dim1, dims...; storage))
+randn(T::Type, dim1::Integer, dims::Integer...; storage::MTL.MTLResourceOptions=Private, kwargs...) =
+    Random.randn!(MtlArray{T}(undef, dim1, dims...; storage); kwargs...)
 
 # untyped out-of-place
-rand(dim1::Integer, dims::Integer...) = Random.rand!(MtlArray{Float32}(undef, dim1, dims...))
-randn(dim1::Integer, dims::Integer...; kwargs...) = Random.randn!(MtlArray{Float32}(undef, dim1, dims...))
+rand(dim1::Integer, dims::Integer...; storage::MTL.MTLResourceOptions=Private) = Random.rand!(MtlArray{Float32}(undef, dim1, dims...; storage))
+randn(dim1::Integer, dims::Integer...; storage::MTL.MTLResourceOptions=Private, kwargs...) = Random.randn!(MtlArray{Float32}(undef, dim1, dims...; storage); kwargs...)
 
 # seeding
 seed!(seed=Base.rand(UInt64)) = Random.seed!(gpuarrays_rng(), seed)
