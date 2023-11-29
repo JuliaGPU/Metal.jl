f9bf7badbc0712bba049f2f2ef4e82ad611d0130
Tim Besard <tim.besard@gmail.com>
2023-11-29 15:26:08 +0100

    Skip profiling tests on CI.

--- test/profiling.jl
+++ test/profiling.jl
@@ -2,9 +2,15 @@
 mktempdir() do tmpdir
 cd(tmpdir) do
 
+if parse(Bool, get(ENV, "CI", "false"))
+@warn "Skipping profiling tests on CI due to sandboxing issues"
+else
+
 @testset "macro" begin
     Metal.@profile identity(nothing)
     @test isdir("julia_1.trace")
+end
+
 end
 
 end
