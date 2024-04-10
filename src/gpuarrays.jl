7c59650b6a48f2a32681a024893c342e9af8658c
James Schloss <jrs.schloss@gmail.com>
2024-04-10 18:48:40 +0200

    adding necessary changes for KA transition for gpuarrays

--- src/gpuarrays.jl
+++ src/gpuarrays.jl
@@ -2,12 +2,10 @@
 
 GPUArrays.device(x::MtlArray) = x.dev
 
+import KernelAbstractions
+import KernelAbstractions: Backend
 
-## execution
-
-struct mtlArrayBackend <: AbstractGPUBackend end
-
-struct mtlKernelContext <: AbstractKernelContext end
+struct mtlArrayBackend <: Backend end
 
 @inline function GPUArrays.launch_heuristic(::mtlArrayBackend, f::F, args::Vararg{Any,N};
                                              elements::Int, elements_per_thread::Int) where {F,N}
@@ -19,50 +17,6 @@ struct mtlKernelContext <: AbstractKernelContext end
 
     return (; threads=Int(threads), blocks=Int(blocks))
 end
-
-function GPUArrays.gpu_call(::mtlArrayBackend, f, args, threads::Int, groups::Int;
-                            name::Union{String,Nothing})
-    @metal threads groups name f(mtlKernelContext(), args...)
-end
-
-
-## on-device
-
-# indexing
-GPUArrays.blockidx(ctx::mtlKernelContext)     = threadgroup_position_in_grid_1d()
-GPUArrays.blockdim(ctx::mtlKernelContext)     = threads_per_threadgroup_1d()
-GPUArrays.threadidx(ctx::mtlKernelContext)    = thread_position_in_threadgroup_1d()
-GPUArrays.griddim(ctx::mtlKernelContext)      = threadgroups_per_grid_1d()
-GPUArrays.global_index(ctx::mtlKernelContext) = thread_position_in_grid_1d()
-GPUArrays.global_size(ctx::mtlKernelContext)  = threads_per_grid_1d()
-
-# math
-
-@inline GPUArrays.cos(ctx::mtlKernelContext, x)  = cos(x)
-@inline GPUArrays.sin(ctx::mtlKernelContext, x)  = sin(x)
-@inline GPUArrays.sqrt(ctx::mtlKernelContext, x) = sqrt(x)
-@inline GPUArrays.log(ctx::mtlKernelContext, x)  = log(x)
-
-# memory
-
-@inline function GPUArrays.LocalMemory(::mtlKernelContext, ::Type{T}, ::Val{dims}, ::Val{id}
-                                      ) where {T, dims, id}
-    ptr = emit_threadgroup_memory(T, Val(prod(dims)))
-    MtlDeviceArray(dims, ptr)
-end
-
-# synchronization
-
-@inline GPUArrays.synchronize_threads(::mtlKernelContext) =
-    threadgroup_barrier(MemoryFlagThreadGroup)
-
-
-
-#
-# Host abstractions
-#
-
-GPUArrays.backend(::Type{<:MtlArray}) = mtlArrayBackend()
 
 const GLOBAL_RNGs = Dict{MTLDevice,GPUArrays.RNG}()
 function GPUArrays.default_rng(::Type{<:MtlArray})
