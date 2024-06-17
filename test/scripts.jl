f663054233fe4feba22ae6f1de48ea22daec49a8
Tim Besard <tim.besard@gmail.com>
2024-06-17 22:10:42 +0200

    Rework.

--- test/scripts.jl
+++ test/scripts.jl
@@ -3,39 +3,41 @@ using LLVM
 @testset "scripts" begin
 
 mktempdir() do dir
-    dummy = joinpath(@__DIR__, "dummy.metallib")
     metallib_as = joinpath(dirname(@__DIR__), "bin", "metallib-as")
     metallib_dis = joinpath(dirname(@__DIR__), "bin", "metallib-dis")
+    metallib_load = joinpath(dirname(@__DIR__), "bin", "metallib-load")
+
+    # make sure we can load the dummy metallib
+    dummy = joinpath(@__DIR__, "dummy.metallib")
+    @test success(`$metallib_load $dummy`)
 
     # disassemble to bitcode
-    run(`$metallib_dis -o $(dir)/dummy.bc $(dummy)`)
+    run(`$metallib_dis -o $dir/dummy.bc $dummy kernel_1`)
     @test isfile(joinpath(dir, "dummy.bc"))
     @dispose ctx=Context() mod=parse(LLVM.Module, read(joinpath(dir, "dummy.bc"))) begin
         @test mod isa LLVM.Module
     end
 
     # disassemble to IR
-    run(`$metallib_dis -o $(dir)/dummy.ll $(dummy)`)
+    run(`$metallib_dis -o $dir/dummy.ll $dummy kernel_1`)
     @test isfile(joinpath(dir, "dummy.ll"))
     @test contains(read(joinpath(dir, "dummy.ll"), String), "define void @kernel_1()")
 
     # assemble and load bitcode (without downgrading)
-    run(`$metallib_as -f -o $(dir)/dummy.metallib $(dir)/dummy.bc`)
-    let
-        dev = current_device()
-        lib = MTLLibraryFromData(dev, read(joinpath(dir, "dummy.metallib")))
-        fun = MTLFunction(lib, "kernel_1")
-        pipeline = MTLComputePipelineState(dev, fun)
-    end
+    run(`$metallib_as -f -o $dir/dummy.metallib $dir/dummy.bc`)
+    @test success(`$metallib_load $dir/dummy.metallib`)
 
     # assemble and load IR
-    run(`$metallib_as -f -o $(dir)/dummy.metallib $(dir)/dummy.ll`)
-    let
-        dev = current_device()
-        lib = MTLLibraryFromData(dev, read(joinpath(dir, "dummy.metallib")))
-        fun = MTLFunction(lib, "kernel_1")
-        pipeline = MTLComputePipelineState(dev, fun)
-    end
+    run(`$metallib_as -f -o $dir/dummy.metallib $dir/dummy.ll`)
+    @test success(`$metallib_load $dir/dummy.metallib`)
+
+    # test pipelines
+    @test success(pipeline(`$metallib_dis -o - $dummy kernel_1`,
+                           `$metallib_as -o - -`,
+                           `$metallib_load -`))
+    @test success(pipeline(`$metallib_dis -S -o - $dummy kernel_1`,
+                           `$metallib_as -o - -`,
+                           `$metallib_load -`))
 end
 
 end
