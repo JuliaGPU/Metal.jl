5c6ebd15e976764275e21f8c017dbd8c8c6083bf
Tim Gymnich <tgymnich@icloud.com>
2024-07-09 12:31:06 +0200

    add test

--- test/device/intrinsics.jl
+++ test/device/intrinsics.jl
@@ -157,6 +157,11 @@ end
     bufferE = MtlArray(e)
     vecE = Array(SpecialFunctions.erfc.(bufferE))
     @test vecE ≈ SpecialFunctions.erfc.(e)
+
+    f = collect(LinRange(nextfloat(-0.3f0), 0.3f0, 20))
+    bufferF = MtlArray(f)
+    vecF = Array(SpecialFunctions.erfinv.(bufferF))
+    @test vecF ≈ SpecialFunctions.erfinv.(f)
 end
 
 ############################################################################################
