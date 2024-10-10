55e88e36f9190e1a2ae6344c27984d1e0170c8e4
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-10-10 16:13:10 -0300

    Only load BFloat16sExt on Apple systems (#454)

--- ext/BFloat16sExt.jl
+++ ext/BFloat16sExt.jl
@@ -4,9 +4,11 @@ using Metal: MPS.MPSDataType, MPS.MPSDataTypeBFloat16, MPS.jl_mps_to_typ, macos_
 using BFloat16s
 
 # BFloat is only supported in MPS starting in MacOS 14
-if macos_version() >= v"14"
-    Base.convert(::Type{MPSDataType}, ::Type{BFloat16}) = MPSDataTypeBFloat16
-    jl_mps_to_typ[MPSDataTypeBFloat16] = BFloat16
+@static if Sys.isapple()
+    if macos_version() >= v"14"
+        Base.convert(::Type{MPSDataType}, ::Type{BFloat16}) = MPSDataTypeBFloat16
+        jl_mps_to_typ[MPSDataTypeBFloat16] = BFloat16
+    end
 end
 
 end # module
