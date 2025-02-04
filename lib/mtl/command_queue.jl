85ae354655582a12e994e4efb24181e0f33d9d10
Tim Gymnich <tgymnich@icloud.com>
2025-02-05 00:48:47 +0100

    add version checks

--- lib/mtl/command_queue.jl
+++ lib/mtl/command_queue.jl
@@ -1,13 +1,23 @@
-export MTLCommandQueueDescriptor
 
-# @objcwrapper immutable=false MTLCommandQueueDescriptor <: NSObject
+@static if Metal.is_macos(v"15.0.0")
+    export MTLCommandQueueDescriptor
 
-function MTLCommandQueueDescriptor()
-    handle = @objc [MTLCommandQueueDescriptor alloc]::id{MTLCommandQueueDescriptor}
-    obj = MTLCommandQueueDescriptor(handle)
-    finalizer(release, obj)
-    @objc [obj::id{MTLCommandQueueDescriptor} init]::id{MTLCommandQueueDescriptor}
-    return obj
+    # @objcwrapper immutable=false MTLCommandQueueDescriptor <: NSObject
+
+    function MTLCommandQueueDescriptor()
+        handle = @objc [MTLCommandQueueDescriptor alloc]::id{MTLCommandQueueDescriptor}
+        obj = MTLCommandQueueDescriptor(handle)
+        finalizer(release, obj)
+        @objc [obj::id{MTLCommandQueueDescriptor} init]::id{MTLCommandQueueDescriptor}
+        return obj
+    end
+
+    function MTLCommandQueue(dev::MTLDevice, descriptor::MTLCommandQueueDescriptor)
+        handle = @objc [dev::id{MTLDevice} newCommandQueueWithDescriptor:descriptor::id{MTLCommandQueueDescriptor}]::id{MTLCommandQueue}
+        obj = MTLCommandQueue(handle)
+        finalizer(release, obj)
+        return obj
+    end
 end
 
 
@@ -22,9 +32,3 @@ function MTLCommandQueue(dev::MTLDevice)
     return obj
 end
 
-function MTLCommandQueue(dev::MTLDevice, descriptor::MTLCommandQueueDescriptor)
-    handle = @objc [dev::id{MTLDevice} newCommandQueueWithDescriptor:descriptor::id{MTLCommandQueueDescriptor}]::id{MTLCommandQueue}
-    obj = MTLCommandQueue(handle)
-    finalizer(release, obj)
-    return obj
-end
