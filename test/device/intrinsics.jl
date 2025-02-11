78ba579c71d059ba6e4e0fb8f9c7734a8b56c93d
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-02-10 21:27:12 -0400

    Only test nextafter intrinsic when available.
    Tests should still pass when run on macOS 13

--- test/device/intrinsics.jl
+++ test/device/intrinsics.jl
@@ -355,20 +355,33 @@ end
 
 
     let # nextafter
-        N = 4
         function nextafter_test(X, y)
             idx = thread_position_in_grid_1d()
             X[idx] = Metal.nextafter(X[idx], y)
             return nothing
         end
+
+        # Check the code is generated as expected
+        outval = T(0)
+        function nextafter_out_test()
+            Metal.nextafter(outval, outval)
+            return
+        end
+
+        N = 4
         arr = rand(T, N)
 
-        # test the intrinsic
-        buffer1 = MtlArray(arr)
-        Metal.@sync @metal threads = N nextafter_test(buffer1, typemax(T))
-        @test Array(buffer1) == nextfloat.(arr)
-        Metal.@sync @metal threads = N nextafter_test(buffer1, typemin(T))
-        @test Array(buffer1) == arr
+        # test the intrinsic (macOS >= v14)
+        if metal_support() >= v"3.1"
+            buffer1 = MtlArray(arr)
+            Metal.@sync @metal threads = N nextafter_test(buffer1, typemax(T))
+            @test Array(buffer1) == nextfloat.(arr)
+            Metal.@sync @metal threads = N nextafter_test(buffer1, typemin(T))
+            @test Array(buffer1) == arr
+
+            ir = sprint(io->(@device_code_llvm io=io dump_module=true @metal nextafter_out_test()))
+            @test occursin(Regex("@air\\.nextafter\\.f$(8*sizeof(T))"), ir)
+        end
 
         # test for metal < 3.1
         buffer2 = MtlArray(arr)
@@ -377,14 +390,6 @@ end
         Metal.@sync @metal threads = N metal = v"3.0" nextafter_test(buffer2, typemin(T))
         @test Array(buffer2) == arr
 
-        # Check the code is generated as expected
-        outval = T(0)
-        function nextafter_out_test()
-            Metal.nextafter(outval, outval)
-            return
-        end
-        ir = sprint(io->(@device_code_llvm io=io dump_module=true @metal nextafter_out_test()))
-        @test occursin(Regex("@air\\.nextafter\\.f$(8*sizeof(T))"), ir)
         ir = sprint(io->(@device_code_llvm io=io dump_module=true @metal metal = v"3.0" nextafter_out_test()))
         @test occursin(Regex("@air\\.sign\\.f$(8*sizeof(T))"), ir)
     end
