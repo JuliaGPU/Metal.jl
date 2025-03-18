e2fd80d4429c00378c0887b935c20281e4dde12b
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-03-18 17:46:46 -0300

    Remove copying

--- lib/mpsgraphs/matmul.jl
+++ lib/mpsgraphs/matmul.jl
@@ -1,8 +1,14 @@
-function _matmul!(c::MPSMatrix, ::Type{Tc}, a::MPSMatrix, b::MPSMatrix, ::Type{Tab}, alpha::Number, beta::Number, transpose_a, transpose_b) where {Tc, Tab}
+function _matmul!(c::MtlArray{Tc}, a::MtlArray{Tab}, b::MtlArray{Tab}, alpha::Number, beta::Number, transpose_a, transpose_b) where {Tc, Tab}
     graph = MPSGraph()
 
     placeA = placeholderTensor(graph, size(a), Tab)
     placeB = placeholderTensor(graph, size(b), Tab)
+    outputTensorData = MPSGraphTensorData(c)
+
+    feeds = Dict{MPSGraphTensor, MPSGraphTensorData}(
+        placeA => MPSGraphTensorData(a),
+        placeB => MPSGraphTensorData(b)
+    )
 
     castA, castB = if Tc != Tab
         castTensor(graph, placeA, Tc, "castA"),
@@ -32,51 +38,32 @@ function _matmul!(c::MPSMatrix, ::Type{Tc}, a::MPSMatrix, b::MPSMatrix, ::Type{T
         multiplicationWithPrimaryTensor(graph, alphatensor, matmul)
     end
 
-    feeds = Dict{MPSGraphTensor, MPSGraphTensorData}(
-        placeA => MPSGraphTensorData(a),
-        placeB => MPSGraphTensorData(b)
-    )
-
     afterbeta = if beta == 0
         afteralpha
     else
         placeC = placeholderTensor(graph, size(c), Tc)
-        feeds[placeC] = MPSGraphTensorData(c)
+        feeds[placeC] = outputTensorData
         betatensor = constantWithScalar(graph, beta, Tc)
         betaC = multiplicationWithPrimaryTensor(graph, betatensor, placeC)
         additionWithPrimaryTensor(graph, afteralpha, betaC)
     end
 
-    # Encode and commit matmul kernel
+    resultdict = Dict{MPSGraphTensor, MPSGraphTensorData}(
+        afterbeta => outputTensorData
+    )
+
     cmdbuf = MPSCommandBuffer(Metal.global_queue(device()))
-    resultdict = encode!(cmdbuf, graph, NSDictionary(feeds), NSArray([afterbeta]))
-    commitAndContinue!(cmdbuf)
+    encode!(cmdbuf, graph, NSDictionary(feeds), NSDictionary(resultdict))
+    commit!(cmdbuf)
+    wait_completed(cmdbuf)
 
-    resultdata = MPSGraphTensorData(id{MPSGraphTensorData}(resultdict[afterbeta]))
-
-    return cmdbuf, MPSNDArray(resultdata)
+    return c
 end
 
 function graph_matmul!(c::MtlArray{Tc, N}, a::MtlArray{Tab, N}, b::MtlArray{Tab, N}, alpha::Number = true, beta::Number = false, transpose_a = false, transpose_b = false) where {Tc, Tab, N}
-    cmdbuf, resultndarr = _matmul!(MPSMatrix(c), Tc, MPSMatrix(a), MPSMatrix(b), Tab, alpha, beta, transpose_a, transpose_b)
-
-    commit!(cmdbuf) do cmdBuf
-        exportDataWithCommandBuffer(resultndarr, cmdBuf, c.data[], Tc, c.offset)
-    end
-
-    wait_completed(cmdbuf)
-
-    return c
+    _matmul!(c, a, b, alpha, beta, transpose_a, transpose_b)
 end
 
 function graph_matvecmul!(c::MtlVector{Tc}, a::MtlMatrix{Tab}, b::MtlVector{Tab}, alpha::Number = true, beta::Number = false, transpose = false) where {Tc, Tab}
-    cmdbuf, resultndarr = _matmul!(MPSMatrix(c), Tc, MPSMatrix(a), MPSMatrix(b), Tab, alpha, beta, transpose, false)
-
-    commit!(cmdbuf) do cmdBuf
-        exportDataWithCommandBuffer(resultndarr, cmdBuf, c.data[], Tc, c.offset)
-    end
-
-    wait_completed(cmdbuf)
-
-    return c
+    _matmul!(c, a, b, alpha, beta, transpose, false)
 end
