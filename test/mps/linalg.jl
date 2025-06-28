637f4a8d06abb72778514992d092e109794ed016
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-06-28 11:11:31 -0300

    Synchronize resources before cpu access of `ManagedStorage` resource
    Also a few test tweaks

--- test/mps/linalg.jl
+++ test/mps/linalg.jl
@@ -14,20 +14,22 @@ if MPS.is_supported(device())
     alpha = Float64(1)
     beta  = Float64(1)
 
-    @testset "$(input_jl_type) => $accum_jl_type" for (input_jl_type, accum_jl_type) in MPS.MPS_VALID_MATMUL_TYPES
-        arr_a = rand(input_jl_type, (rows_a,cols_a))
-        arr_b = rand(input_jl_type, (rows_b,cols_b))
-        arr_c = zeros(accum_jl_type, (rows_c,cols_c))
+    for (input_jl_type, accum_jl_type) in MPS.MPS_VALID_MATMUL_TYPES
+        @testset let input_jl_type = input_jl_type, accum_jl_type = accum_jl_type
+            arr_a = rand(input_jl_type, (rows_a,cols_a))
+            arr_b = rand(input_jl_type, (rows_b,cols_b))
+            arr_c = zeros(accum_jl_type, (rows_c,cols_c))
 
-        buf_a = MtlArray{input_jl_type}(arr_a)
-        buf_b = MtlArray{input_jl_type}(arr_b)
-        buf_c = MtlArray{accum_jl_type}(undef, (rows_c,cols_c))
+            buf_a = MtlArray{input_jl_type}(arr_a)
+            buf_b = MtlArray{input_jl_type}(arr_b)
+            buf_c = MtlArray{accum_jl_type}(undef, (rows_c,cols_c))
 
-        truth_c = (alpha .* accum_jl_type.(arr_a)) *  accum_jl_type.(arr_b) .+ (beta .* arr_c)
+            truth_c = (alpha .* accum_jl_type.(arr_a)) *  accum_jl_type.(arr_b) .+ (beta .* arr_c)
 
-        MPS.matmul!(buf_c, buf_a, buf_b, alpha, beta)
+            MPS.matmul!(buf_c, buf_a, buf_b, alpha, beta)
 
-        @test all(Array(buf_c) .≈ truth_c)
+            @test Array(buf_c) ≈ truth_c
+        end
     end
 end
 
@@ -49,23 +51,25 @@ end
     alpha = Float64(1)
     beta = Float64(1)
 
-    @testset "$(input_jl_type) => $accum_jl_type" for (input_jl_type, accum_jl_type) in MPS.MPS_VALID_MATMUL_TYPES
-        arr_a = rand(input_jl_type, (rows_a, cols_a, batch_size))
-        arr_b = rand(input_jl_type, (rows_b, cols_b, batch_size))
-        arr_c = zeros(accum_jl_type, (rows_c, cols_c, batch_size))
+    for (input_jl_type, accum_jl_type) in MPS.MPS_VALID_MATMUL_TYPES
+        @testset let input_jl_type = input_jl_type, accum_jl_type = accum_jl_type
+            arr_a = rand(input_jl_type, (rows_a, cols_a, batch_size))
+            arr_b = rand(input_jl_type, (rows_b, cols_b, batch_size))
+            arr_c = zeros(accum_jl_type, (rows_c, cols_c, batch_size))
 
-        buf_a = MtlArray{input_jl_type}(arr_a)
-        buf_b = MtlArray{input_jl_type}(arr_b)
-        buf_c = MtlArray{accum_jl_type}(undef, (rows_c, cols_c, batch_size))
+            buf_a = MtlArray{input_jl_type}(arr_a)
+            buf_b = MtlArray{input_jl_type}(arr_b)
+            buf_c = MtlArray{accum_jl_type}(undef, (rows_c, cols_c, batch_size))
 
-        truth_c = Array{accum_jl_type}(undef, (rows_c, cols_c, batch_size))
-        for i in 1:batch_size
-            @views truth_c[:, :, i] = (alpha .* accum_jl_type.(arr_a[:, :, i])) * accum_jl_type.(arr_b[:, :, i]) .+ (beta .* arr_c[:, :, i])
+            truth_c = Array{accum_jl_type}(undef, (rows_c, cols_c, batch_size))
+            for i in 1:batch_size
+                @views truth_c[:, :, i] = (alpha .* accum_jl_type.(arr_a[:, :, i])) * accum_jl_type.(arr_b[:, :, i]) .+ (beta .* arr_c[:, :, i])
+            end
+
+            MPS.matmul!(buf_c, buf_a, buf_b, alpha, beta)
+
+            @test all(Array(buf_c) .≈ truth_c)
         end
-
-        MPS.matmul!(buf_c, buf_a, buf_b, alpha, beta)
-
-        @test all(Array(buf_c) .≈ truth_c)
     end
 end
 
