faa176fed8d2e4845aa864027014924a6bd5a126
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-07-08 22:55:55 -0300

    More barrier functionality

--- lib/mtl/command_enc4.jl
+++ lib/mtl/command_enc4.jl
@@ -2,9 +2,21 @@ export endEncoding!, barrierAfterStages!
 
 # @objcwrapper immutable=true MTL4CommandEncoder <: NSObject
 
-function barrierAfterStages!(encoder::MTL4CommandEncoder, afterStages::MTLStages=MTLStageAll, queueStages::MTLStages=MTLStageAll, visibilityOptions::MTL4VisibilityOptions=MTL4VisibilityOptionDevice)
+function barrierAfterEncoderStages!(encoder::MTL4CommandEncoder, afterEncoderStages::MTLStages=MTLStageAll, beforeEncoderStages::MTLStages=MTLStageAll, visibilityOptions::MTL4VisibilityOptions=MTL4VisibilityOptionResourceAlias)
+    @objc [encoder::id{MTL4CommandEncoder} barrierAfterEncoderStages:afterEncoderStages::MTLStages
+                                            beforeEncoderStages:beforeEncoderStages::MTLStages
+                                            visibilityOptions:visibilityOptions::MTL4VisibilityOptions]::Nothing
+end
+
+function barrierAfterQueueStages!(encoder::MTL4CommandEncoder, afterQueueStages::MTLStages=MTLStageAll, beforeStages::MTLStages=MTLStageAll, visibilityOptions::MTL4VisibilityOptions=MTL4VisibilityOptionResourceAlias)
+    @objc [encoder::id{MTL4CommandEncoder} barrierAfterQueueStages:afterQueueStages::MTLStages
+                                            beforeStages:beforeStages::MTLStages
+                                            visibilityOptions:visibilityOptions::MTL4VisibilityOptions]::Nothing
+end
+
+function barrierAfterStages!(encoder::MTL4CommandEncoder, afterStages::MTLStages=MTLStageAll, beforeQueueStages::MTLStages=MTLStageAll, visibilityOptions::MTL4VisibilityOptions=MTL4VisibilityOptionResourceAlias)
     @objc [encoder::id{MTL4CommandEncoder} barrierAfterStages:afterStages::MTLStages
-                                            beforeQueueStages:queueStages::MTLStages
+                                            beforeQueueStages:beforeQueueStages::MTLStages
                                             visibilityOptions:visibilityOptions::MTL4VisibilityOptions]::Nothing
 end
 
