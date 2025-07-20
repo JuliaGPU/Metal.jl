5838fe93b4d9c699a1b0558e6d732173490a5d8d
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-07-20 19:53:45 -0300

    Oops

--- lib/mpsgraphs/core.jl
+++ lib/mpsgraphs/core.jl
@@ -1,8 +1,5 @@
 # Contains definitions for api from MPSGraphCore.h, MPSGraphDevice.h
 
-MPSGraphDevice(dev) = MPSGraphDevice(@objc [MPSGraphDevice deviceWithMTLDevice:dev::id{MTLDevice}]::id{MPSGraphDevice})
-
-
 ## MPSGraphCore.h
 # @objcwrapper MPSGraphObject <: NSObject
 # @objcwrapper MPSGraphType <: MPSGraphObject
