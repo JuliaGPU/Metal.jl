4bc3ceab2af7b3c4d781f48d2d1e31683b82723d
Simeon David Schaub <simeon@schaub.rocks>
2025-09-05 15:38:09 +0200

    wip

---
+++ test/device/random.jl
@@ -0,0 +1,13 @@
+@testset "device-side rng" begin
+    function rand_kernel!(a)
+        i = thread_position_in_grid_1d()
+        a[i] = rand(Float32)
+        return
+    end
+
+    n = 128
+    a = Metal.fill!(-1f0, n)
+    @metal threads=n rand_kernel!(a)
+    @test all(0 .<= a .< 1)
+    @test length(unique(Array(a))) == n
+end
