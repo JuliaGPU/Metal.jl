db4a3d3c0378636f44e0e983de1608ff7261f498
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-09-18 15:59:41 -0300

    KernelIntrinsics

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -7,6 +7,8 @@ skip_tests = Set([
     "Convert",           # depends on https://github.com/JuliaGPU/Metal.jl/issues/69
     "SpecialFunctions",  # gamma and erfc not currently supported on Metal.jl
     "sparse",            # not supported yet
+    "CPU synchronization",
+    "fallback test: callable types",
 ])
 if Metal.is_virtual(Metal.device())
     # device-side printing needs GPU logging, which is unsupported on virtualized GPUs
