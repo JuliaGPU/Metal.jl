49b8a92ec30c3d2e7e1babbf2951d0284eba3fa9
Tim Besard <tim.besard@gmail.com>
2025-10-09 08:44:10 +0200

    Simplify docs.

--- src/device/intrinsics/arguments.jl
+++ src/device/intrinsics/arguments.jl
@@ -149,99 +149,56 @@ Return the thread execution width of a simdgroup.
 
 # Dimensioned intrinsics
 
-# helper macro for dimensioned intrinsics
-macro doc_dim(docs, base_name)
-    _1d_name = Symbol(string(base_name) * "_1d")
-    _2d_name = Symbol(string(base_name) * "_2d")
-    _3d_name = Symbol(string(base_name) * "_3d")
-
-    expr = quote
-        @doc $docs $base_name
-        @doc $docs $_1d_name
-        @doc $docs $_2d_name
-        @doc $docs $_3d_name
-    end
-    esc(expr)
-end
-
-@doc_dim """
+@doc """
     dispatch_threads_per_threadgroup()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    dispatch_threads_per_threadgroup_1d()::UInt32
-    dispatch_threads_per_threadgroup_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    dispatch_threads_per_threadgroup_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the thread execution width specified at dispatch for a threadgroup.
 """ dispatch_threads_per_threadgroup
 
-@doc_dim """
+@doc """
     grid_origin()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    grid_origin_1d()::UInt32
-    grid_origin_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    grid_origin_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the origin offset of the grid for threads that read per-thread stage-in data.
 """ grid_origin
 
-@doc_dim """
+@doc """
     grid_size()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    grid_size_1d()::UInt32
-    grid_size_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    grid_size_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return maximum size of the grid for threads that read per-thread stage-in data.
 """ grid_size
 
-@doc_dim """
+@doc """
     thread_position_in_threadgroup()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    thread_position_in_threadgroup_1d()::UInt32
-    thread_position_in_threadgroup_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    thread_position_in_threadgroup_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the current thread's unique position within a threadgroup.
 """ thread_position_in_threadgroup
 
-@doc_dim """
+@doc """
     thread_position_in_grid()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    thread_position_in_grid_1d()::UInt32
-    thread_position_in_grid_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    thread_position_in_grid_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the current thread's position in an N-dimensional grid of threads.
 """ thread_position_in_grid
 
-@doc_dim """
+@doc """
     threadgroup_position_in_grid()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    threadgroup_position_in_grid_1d()::UInt32
-    threadgroup_position_in_grid_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    threadgroup_position_in_grid_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the current threadgroup's unique position within the grid.
 """ threadgroup_position_in_grid
 
-@doc_dim """
+@doc """
     threadgroups_per_grid()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    threadgroups_per_grid_1d()::UInt32
-    threadgroups_per_grid_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    threadgroups_per_grid_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the number of threadgroups per grid.
 """ threadgroups_per_grid
 
-@doc_dim """
+@doc """
     threads_per_grid()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-    threads_per_grid_1d()::UInt32
-    threads_per_grid_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    threads_per_grid_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the grid size.
 """ threads_per_grid
 
-@doc_dim """
+@doc """
     threads_per_threadgroup()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
-
-    threads_per_threadgroup_1d()::UInt32
-    threads_per_threadgroup_2d()::@NamedTuple{x::UInt32, y::UInt32}
-    threads_per_threadgroup_3d()::@NamedTuple{x::UInt32, y::UInt32, z::UInt32}
 
 Return the thread execution width of a threadgroup.
 """ threads_per_threadgroup
