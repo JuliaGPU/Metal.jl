3ef42d97a75ff1a5b25ba8cad13ee7d6c1b70ae6
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-01-28 21:25:09 -0400

    Use Metal.jl native rand by default (#727)
    * Add regression tests

--- src/random.jl
+++ src/random.jl
@@ -227,26 +227,27 @@ Random.randn(rng::RNG, T::Type=Float32) = Random.randn(rng, T, 1)[]
 # resolve ambiguities
 Random.randn(rng::RNG, T::Random.BitFloatType) = Random.randn(rng, T, 1)[]
 
+############################################
+#            RNG-less API                  #
+# Use MPS for uniformly distributed RNG,   #
+# but native rand for normally distributed #
+# to work around JuliaGPU/Metal.jl#474     #
+############################################
 
 # GPUArrays in-place
 Random.rand!(A::MtlArray) = Random.rand!(mtl_rng(), A)
 Random.randn!(A::MtlArray) = Random.randn!(mtl_rng(), A)
 
-# Use MPS random functionality where possible
+# Use MPS random functionality where possible for uniformly distributed RNG
 function Random.rand!(A::MPS.UniformArray)
     return Random.rand!(mpsrand_rng(), A)
-end
-function Random.randn!(A::MPS.NormalArray)
-    return Random.randn!(mpsrand_rng(), A)
 end
 
 # GPUArrays out-of-place
 function rand(T::MPS.UniformType, dims::Dims; storage=DefaultStorageMode)
     return Random.rand!(mpsrand_rng(), MtlArray{T,length(dims),storage}(undef, dims...))
 end
-function randn(T::MPS.NormalType, dims::Dims; storage=DefaultStorageMode)
-    return Random.randn!(mpsrand_rng(), MtlArray{T,length(dims),storage}(undef, dims...))
-end
+
 rand(T::Type, dims::Dims; storage=DefaultStorageMode) =
     Random.rand!(mtl_rng(), MtlArray{T,length(dims),storage}(undef, dims...))
 randn(T::Type, dims::Dims; storage=DefaultStorageMode) =
@@ -255,9 +256,6 @@ randn(T::Type, dims::Dims; storage=DefaultStorageMode) =
 # support all dimension specifications
 function rand(T::MPS.UniformType, dim1::Integer, dims::Integer...; storage=DefaultStorageMode)
     return Random.rand!(mpsrand_rng(), MtlArray{T,length(dims) + 1,storage}(undef, dim1, dims...))
-end
-function randn(T::MPS.NormalType, dim1::Integer, dims::Integer...; storage=DefaultStorageMode)
-    return Random.randn!(mpsrand_rng(), MtlArray{T,length(dims) + 1,storage}(undef, dim1, dims...))
 end
 
 rand(T::Type, dim1::Integer, dims::Integer...; storage=DefaultStorageMode) =
@@ -269,7 +267,7 @@ randn(T::Type, dim1::Integer, dims::Integer...; storage=DefaultStorageMode) =
 rand(dim1::Integer, dims::Integer...; storage=DefaultStorageMode) =
     Random.rand!(mpsrand_rng(), MtlArray{Float32,length(dims) + 1,storage}(undef, dim1, dims...))
 randn(dim1::Integer, dims::Integer...; storage=DefaultStorageMode) =
-    Random.randn!(mpsrand_rng(), MtlArray{Float32,length(dims) + 1,storage}(undef, dim1, dims...))
+    Random.randn!(mtl_rng(), MtlArray{Float32,length(dims) + 1,storage}(undef, dim1, dims...))
 
 # scalars
 rand(T::Type=Float32; storage=SharedStorage) = rand(T, 1; storage)[1]
