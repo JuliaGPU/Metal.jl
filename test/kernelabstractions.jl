2573d1f40d63eef8243c1efe2deb5dea4a553f70
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-06-03 18:51:41 -0300

    Include KA Convert tests

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -4,7 +4,6 @@ using Metal.MetalKernels
 include(joinpath(dirname(pathof(KernelAbstractions)), "..", "test", "testsuite.jl"))
 
 skip_tests = Set([
-    "Convert",           # depends on https://github.com/JuliaGPU/Metal.jl/issues/69
     "SpecialFunctions",  # gamma and erfc not currently supported on Metal.jl
     "sparse",            # not supported yet
     "CPU synchronization",
