5b3d23088f2a0c1abfbc3b65f610064e1c381f49
Tim Besard <tim.besard@gmail.com>
2026-06-08 10:14:56 +0200

    Remove dump_graph.

--- lib/mpsgraphs/operations.jl
+++ lib/mpsgraphs/operations.jl
@@ -64,38 +64,3 @@ function identityWithTensor(graph::MPSGraph, tensor::MPSGraphTensor, name = "ide
                                 name:name::id{NSString}]::id{MPSGraphTensor}
     MPSGraphTensor(obj)
 end
-
-"""
-    dump_graph(graph::MPSGraph)
-
-Dumps the `graph`.
-
-!!! warning
-    This function is undocumented from Apple so it may stop working at any time.
-"""
-dump_graph(graph::MPSGraph) = @objc [graph::id{MPSGraph} dump]::Nothing ## COV_EXCL_LINE
-
-# Scaled dot-product attention: softmax((Q · K^T) * scale [+ mask]) · V, fused.
-# Available macOS 14.0+.
-function scaledDotProductAttentionWithQueryTensor(graph::MPSGraph, Q::MPSGraphTensor,
-                                                  K::MPSGraphTensor, V::MPSGraphTensor,
-                                                  scale::Real, name = "sdpa")
-    obj = @objc [graph::id{MPSGraph} scaledDotProductAttentionWithQueryTensor:Q::id{MPSGraphTensor}
-                                                            keyTensor:K::id{MPSGraphTensor}
-                                                          valueTensor:V::id{MPSGraphTensor}
-                                                                scale:scale::Cfloat
-                                                                 name:name::id{NSString}]::id{MPSGraphTensor}
-    MPSGraphTensor(obj)
-end
-function scaledDotProductAttentionWithQueryTensor(graph::MPSGraph, Q::MPSGraphTensor,
-                                                  K::MPSGraphTensor, V::MPSGraphTensor,
-                                                  mask::MPSGraphTensor, scale::Real,
-                                                  name = "sdpa")
-    obj = @objc [graph::id{MPSGraph} scaledDotProductAttentionWithQueryTensor:Q::id{MPSGraphTensor}
-                                                            keyTensor:K::id{MPSGraphTensor}
-                                                          valueTensor:V::id{MPSGraphTensor}
-                                                           maskTensor:mask::id{MPSGraphTensor}
-                                                                scale:scale::Cfloat
-                                                                 name:name::id{NSString}]::id{MPSGraphTensor}
-    MPSGraphTensor(obj)
-end
