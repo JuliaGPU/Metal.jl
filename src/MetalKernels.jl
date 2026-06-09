6a5c48d606f02120ae829a7cbddb6672e3d0eb12
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-06-09 17:52:08 -0300

    Fix

--- src/MetalKernels.jl
+++ src/MetalKernels.jl
@@ -1,7 +1,7 @@
 module MetalKernels
 
 using ..Metal
-using ..Metal: @device_override, DefaultStorageMode, SharedStorage, metal_support
+using ..Metal: @device_override, DefaultStorageMode, SharedStorage, PrivateStorage, metal_support
 using GPUCompiler
 
 import KernelAbstractions as KA
