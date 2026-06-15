ab2376ba103d92e24421e1316331a0cd03c24e2f
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-06-15 19:22:30 -0300

    Fixup flopscomp again

--- examples/flopscomp.jl
+++ examples/flopscomp.jl
@@ -1,4 +1,5 @@
-using Metal, GPUArrays, LinearAlgebra, Printf, ScopedValues, AppleAccelerate
+using Metal, GPUArrays, LinearAlgebra, Printf, AppleAccelerate
+using ScopedValues: with
 using Plots
 using Plots.Measures
 using BFloat16s
@@ -22,14 +23,11 @@ n_gpu_cores = Metal.num_gpu_cores()
 PLOT_TITLE = "Matmul peakflops for $(device().name) ($n_gpu_cores GPU cores)"
 
 function cpupeakflops(; n::Integer=4096,
-                        n_batch::Integer=1,
                         inT::DataType=Float32,
                         outT::DataType=inT,
                         ntrials::Integer=4,
                         verify=true)
     t = Base.zeros(Float64, ntrials)
-    n_batch == 1 || @warn "n_batch > 1 not supported for `mul!`, running with n_batch=1"
-    n_batch = 1
     shape = (n, n)
     for i=1:ntrials
         c = zeros(outT, shape...)
@@ -39,41 +37,28 @@ function cpupeakflops(; n::Integer=4096,
         verify && @assert only(unique(Array(c))) == n
     end
 
-    return n_batch*2*Float64(n)^3 / minimum(t)
+    return 2*Float64(n)^3 / minimum(t)
 end
-function _peakflops(f, n, n_batch, inT, outT, ntrials; verify=true)
+function _peakflops(f, n, inT, outT, ntrials; verify=true)
     t = Base.zeros(Float64, ntrials)
-    shape = n_batch == 1 ? (n, n) : (n, n, n_batch)
     for i=1:ntrials
-        c = mtl(zeros(outT, shape...))
-        a = mtl(ones(inT, shape...))
-        b = mtl(ones(inT, shape...))
+        c = mtl(zeros(outT, n, n))
+        a = mtl(ones(inT, n, n))
+        b = mtl(ones(inT, n, n))
         t[i] = @elapsed Metal.@sync f(c, a, b)
         verify && @assert only(unique(Array(c))) == n
     end
 
-    return n_batch*2*Float64(n)^3 / minimum(t)
+    return 2*Float64(n)^3 / minimum(t)
 end
-function gpuarrpeakflops(; n_batch::Integer=1,
-                           kwargs...)
-    n_batch == 1 || @warn "n_batch > 1 not supported for `GPUArrays.generic_matmatmul!`, running with n_batch=1"
-    @with (Metal.matmul_alg => :GPUArrays) defaultpeakflops(; n_batch, kwargs...)
-end
-function defaultpeakflops(; n::Integer=4096,
-                           n_batch::Integer=1,
-                           inT::DataType=Float32,
-                           outT::DataType=inT,
-                           ntrials::Integer=3,
-                           verify=true)
-    _peakflops(n, n_batch, inT, outT, ntrials; verify) do c, a, b
-        LinearAlgebra.generic_matmatmul!(c, 'N', 'N', a, b, 1, 0)
+function gpupeakflops(alg)
+    (;  n::Integer=4096,
+        inT::DataType=Float32,
+        outT::DataType=inT,
+        ntrials::Integer=3,
+        verify=true) -> _peakflops(n, inT, outT, ntrials; verify) do c, a, b
+        with(() -> LinearAlgebra.generic_matmatmul!(c, 'N', 'N', a, b, 1, 0), Metal.matmul_alg => alg)
     end
-end
-function mpspeakflops(; kwargs...)
-    @with (Metal.matmul_alg => :MPS) defaultpeakflops(; kwargs...)
-end
-function graphpeakflops(; kwargs...)
-    @with (Metal.matmul_alg => :MPSGraph) defaultpeakflops(; kwargs...)
 end
 function anepeakflops(; kwargs...)
     # VERY HACKY
@@ -84,13 +69,13 @@ function anepeakflops(; kwargs...)
     oldDesc = MPSGraphs._default_exec_desc[].compilationDescriptor
 
     MPSGraphs._default_exec_desc[].compilationDescriptor = newDesc
-    res = graphpeakflops(; kwargs...)
+    res = gpupeakflops(:MPSGraph)(; kwargs...)
     MPSGraphs._default_exec_desc[].compilationDescriptor = oldDesc
 
     return res
 end
 
-function compare(Ns, Fs, inT, outT=inT; n_batch=1, ntrials, verbose=!testing)
+function compare(Ns, Fs, inT, outT=inT; ntrials, verbose=!testing)
     results = Dict()
 
     newFs = let
@@ -116,11 +101,11 @@ function compare(Ns, Fs, inT, outT=inT; n_batch=1, ntrials, verbose=!testing)
     prefixstr = "\33[2K\r($inT, $outT) "
     @time "$((inT, outT))" begin
         for n in Ns
-            verify = (n < maxintfloat(real(outT)) && (inT != Float16 || (n < maxintfloat(real(inT)))))
+            verify = ((inT <: Integer ? typemax : maxintfloat)(real(inT)) > n < maxintfloat(real(outT)) && (inT != Float16 || (n < maxintfloat(real(inT)))))
             n_str = "$n: "
             for (f, info_str) in newFs
                 verbose && print(prefixstr, n_str, info_str)
-                push!(results[info_str], f(; inT, outT, n, n_batch, ntrials, verify=(verify && (f != mpspeakflops))))
+                push!(results[info_str], f(; inT, outT, n, ntrials, verify=(verify && (info_str != "MPS"))))
                 GC.gc(false)
             end
         end
@@ -130,18 +115,19 @@ function compare(Ns, Fs, inT, outT=inT; n_batch=1, ntrials, verbose=!testing)
 end
 
 DEFAULT_FS = [
-    (graphpeakflops, "MPSGraph"),
-    (defaultpeakflops, "Default"),
-    (gpuarrpeakflops, "GPUArrays"),
+    (gpupeakflops(:MPSGraph), "MPSGraph"),
+    (gpupeakflops(:auto), "Default"),
+    (gpupeakflops(:native), "Native"),
+    (gpupeakflops(:GPUArrays), "GPUArrays"),
     (cpupeakflops, "CPU (AppleAccelerate)"),
-    (mpspeakflops, "MPS"), # Run last to prevent different line colours
+    # (gpupeakflops(:MPS), "MPS"), # Run last to prevent different line colours
     # (anepeakflops, "MPSGraph (ANE)"), # Run last to prevent different line colours
 ]
 
-function runcomparison(; Ns=DEFAULT_NS, Fs=DEFAULT_FS, n_batch=1, ntrials=5, verbose=true)
+function runcomparison(; Ns=DEFAULT_NS, Fs=DEFAULT_FS, ntrials=5, verbose=true)
     res = Dict()
     for (inT, outT) in Ts
-        res[(inT,outT)] = (n_batch, Ns, compare(Ns, Fs, inT, outT; n_batch, ntrials, verbose))
+        res[(inT,outT)] = (Ns, compare(Ns, Fs, inT, outT; ntrials, verbose))
     end
     return res
 end
@@ -151,12 +137,10 @@ function plot_results(res, Fs=DEFAULT_FS; outpath=nothing, fileext="svg", plt_ti
     ylim_upper = 9e12
     resplts = []
 
-    n_batches = []
-
     for (inT, outT) in Ts
-        n_batch, Ns, tmpres = res[(inT,outT)]
+        Ns, tmpres = res[(inT,outT)]
 
-        plt = plot(xlabel="N, n_batch=$(n_batch)", legendtitle="($inT, $outT)")
+        plt = plot(xlabel="N", legendtitle="($inT, $outT)")
         for info_str in Fs
             haskey(tmpres, info_str) || continue
 
@@ -168,7 +152,6 @@ function plot_results(res, Fs=DEFAULT_FS; outpath=nothing, fileext="svg", plt_ti
             plot!(plt, Ns, tmpres[info_str]; linewidth=1.5, label="$(peakf) peak: $info_str", α=0.8)
         end
         push!(resplts, plt)
-        push!(n_batches, n_batch)
     end
 
     layout = (cld(length(Ts), plot_width), plot_width)
@@ -179,7 +162,7 @@ function plot_results(res, Fs=DEFAULT_FS; outpath=nothing, fileext="svg", plt_ti
                      bottommargin=15pt,
                      size=(500*layout[2],500*layout[1]))
     if !isnothing(outpath)
-        savefig(plot(finalplot, dpi=500), joinpath(outpath, "bench_all_$(first(n_batches)).$fileext"))
+        savefig(plot(finalplot, dpi=500), joinpath(outpath, "bench_all.$fileext"))
     end
     return finalplot
 end
