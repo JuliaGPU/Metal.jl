1a434866edac2c7739d93ea45081789a0b8453c0
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-09-14 21:46:58 -0300

    Move precompilation guards together
    Only precompile on supported macOS

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,6 +1,6 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
-Sys.isapple() && Sys.ARCH === :aarch64 && @setup_workload begin
+@setup_workload begin
     metallib_file = joinpath(dirname(@__DIR__), "test", "dummy.metallib")
 
     @compile_workload begin
