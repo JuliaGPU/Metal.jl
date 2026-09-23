b7b8abb419d366257184d3799069ddf02f83c563
Tim Besard <tim.besard@gmail.com>
2026-09-23 15:15:24 +0200

    Stop excluding device code from coverage
    GPUCompiler now records line coverage of device code when compiling it
    (JuliaGPU/GPUCompiler.jl#844, in every 2.x release), so kernels no longer
    show up as uncovered. Drop the COV_EXCL markers around device code and the
    src/device codecov ignore, so reports show which device code the tests
    actually compile. Markers around host code that tests can't reach, like the
    error description tables, stay.

--- src/accumulate.jl
+++ src/accumulate.jl
@@ -1,4 +1,3 @@
-## COV_EXCL_START
 function partial_scan(op::Function, output::AbstractArray{T}, input::AbstractArray,
                       Rdim, Rpre, Rpost, Rother, neutral, init,
                       ::Val{maxthreads}, ::Val{inclusive}=Val(true)) where {T, maxthreads, inclusive}
@@ -101,7 +100,6 @@ function aggregate_partial_scan(op::Function, output::AbstractArray, aggregates:
 
     return
 end
-## COV_EXCL_STOP
 
 function scan!(f::Function, output::WrappedMtlArray{T}, input::WrappedMtlArray;
                dims::Integer, init=nothing, neutral=GPUArrays.neutral_element(f, T)) where {T}
